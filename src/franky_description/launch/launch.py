cfeb877cfc7812c5b50fdb4f7898ff9d411ff9c5
prabudhgautam <prabudhrocky2003@gmail.com>
2026-08-14 02:41:18 +0000

    made launch file

--- src/franky_description/launch/launch.py
+++ src/franky_description/launch/launch.py
@@ -1,15 +1,44 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-def generate_launch_description:
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
 
+def generate_launch_description():
+    
+    #instruction to declare the argument model
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(get_package_share_directory("franky_description"), "URDF", "franky.urdf.xacro")
+        description="Absolute path to robot URDF file "
+    ) 
+    
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+
+    # #instruction for starting state publishers
     robot_state_publisher = Node(
         package="robot_state_publisher",
-        executable="robot_state_publisher"
+        executable="robot_state_publisher",
         parameter=[{"robot_description": robot_description}]
     )
+    
+    joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
 
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2"
+        output="screen"
+        arguments=["-d", os.path.join(get_package_share_directory("franky_description"), "rviz", "display.rviz")]
+    )
     return LaunchDescription(
-
-
+        model_arg,
+        robot_state_publisher,
+        joint_state_publisher_gui,
+        rviz_node
     )
