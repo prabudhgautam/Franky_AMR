ab1639e2237718dac9d52ea51a0bdb75b90dd87d
prabudhgautam <prabudhrocky2003@gmail.com>
2026-08-16 21:41:59 +0000

    added controller plugin

--- src/franky_description/launch/gazebo.launch.py
+++ src/franky_description/launch/gazebo.launch.py
@@ -4,7 +4,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription, TimerAction
-from ament_index_python.packages import get_package_share_directory
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -12,8 +11,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
 
     franky_description_dir = get_package_share_directory("franky_description")
-    ros_distro = os.environ["ROS_DISTRO"]
-    is_ignition = "True" if ros_distro == "jazzy" else "False"
 
     model_arg = DeclareLaunchArgument(name="model",default_value=os.path.join(
                                         franky_description_dir, "URDF", "franky.urdf.xacro"
@@ -23,8 +20,6 @@ def generate_launch_description():
     robot_description = ParameterValue(Command([
            "xacro ",
            LaunchConfiguration("model"),
-           " is_ignition:=",
-           is_ignition
            ]),
            value_type=str,
     )
