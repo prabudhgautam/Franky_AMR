0cfafb3e888a5aa6930c6144c0e6269bd28a0148
prabudhgautam <prabudhrocky2003@gmail.com>
2026-08-16 23:42:05 +0000

    added controller plugin

--- src/franky_controller/launch/controller.launch.py
+++ src/franky_controller/launch/controller.launch.py
@@ -1,10 +1,17 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import TimerAction
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
+    controllers_file = os.path.join(
+        get_package_share_directory("franky_controller"),
+        "config",
+        "franky_controllers.yaml",
+    )
 
-    # Wait before spawning, Gazebo's controller_manager should be running
     joint_state_broadcaster_spawner = TimerAction(
         period=3.0,
         actions=[
@@ -14,14 +21,18 @@ def generate_launch_description():
                 arguments=[
                     "joint_state_broadcaster",
                     "--controller-manager",
-                    "/controller_manager"
+                    "/controller_manager",
+                    "--param-file",
+                    controllers_file,
+                    "--controller-manager-timeout",
+                    "60",
                 ],
-                output="screen"
+                output="screen",
             )
-        ]
+        ],
     )
 
-    simple_controller = TimerAction(
+    simple_controller_spawner = TimerAction(
         period=4.0,
         actions=[
             Node(
@@ -30,14 +41,18 @@ def generate_launch_description():
                 arguments=[
                     "simple_velocity_controller",
                     "--controller-manager",
-                    "/controller_manager"
+                    "/controller_manager",
+                    "--param-file",
+                    controllers_file,
+                    "--controller-manager-timeout",
+                    "60",
                 ],
-                output="screen"
+                output="screen",
             )
-        ]
+        ],
     )
 
     return LaunchDescription([
         joint_state_broadcaster_spawner,
-        simple_controller
+        simple_controller_spawner,
     ])
