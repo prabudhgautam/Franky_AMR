002cbc177b3ca9c75bf32224e641a5f57de82a58
prabudhgautam <prabudhrocky2003@gmail.com>
2026-09-17 19:29:29 +0000

    added imu and its bridge

---
+++ src/franky_description/launch/gazebo.launch.py
@@ -0,0 +1,86 @@
+import os
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription, TimerAction
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+
+    franky_description_dir = get_package_share_directory("franky_description")
+
+    model_arg = DeclareLaunchArgument(name="model",default_value=os.path.join(
+                                        franky_description_dir, "URDF", "franky.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot URDF file ") 
+    
+    robot_description = ParameterValue(Command([
+           "xacro ",
+           LaunchConfiguration("model"),
+           ]),
+           value_type=str,
+    )
+    # instruction for starting state publishers #robot_state_publisher is itself a TF broadcaster.
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                      "use_sim_time": True}],
+    )
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(franky_description_dir).parent.resolve())
+        ]
+    )
+
+    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([
+        os.path.join(
+            get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+        launch_arguments=[
+            ("gz_args", [" -v 4", " -r"])
+        ]
+    )
+
+    gz_spawn_entity = TimerAction(
+        period=2.0,
+        actions=[
+            Node(
+                package="ros_gz_sim",
+                executable="create",
+                arguments=["-topic", "robot_description", "-name", "franky"],
+                output="screen"
+            )
+        ]
+    )
+
+    gz_ros2_bridge = TimerAction(
+        period=1.0,
+        actions=[
+            Node(
+                package="ros_gz_bridge",
+                executable="parameter_bridge",
+                arguments=[
+                    "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+                     "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
+                ],
+                remappings=[
+                    ('/imu', '/imu/out'),
+                ],
+                output="screen",
+            )
+        ],
+    )
+
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher_node,
+        gazebo_resource_path,
+        gazebo,
+        gz_spawn_entity,
+        gz_ros2_bridge
+    ])
